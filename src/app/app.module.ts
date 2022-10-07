9577c6ba9b40f8e6cf979d27b442502f84db6dfb
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-07 12:30:38 +0530

    addStudent program added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddStudentComponent } from './add-student/add-student.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddStudentComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
